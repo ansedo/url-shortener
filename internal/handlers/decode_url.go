697c0983e2d479ef23e5f984299032aa164192f7
Sergey Usvyattsev <abro.gafuk@gmail.com>
2022-06-06 17:45:02 +0300

    Add URL parameter validation to `DecodeURL` handler

--- internal/handlers/decode_url.go
+++ internal/handlers/decode_url.go
@@ -4,15 +4,30 @@ import (
 	"github.com/ansedo/url-shortener/internal/services/shortener"
 	"github.com/go-chi/chi/v5"
 	"net/http"
+	"net/url"
 )
 
 func DecodeURL(s *shortener.Shortener) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		uri, err := s.Storage.Get(chi.URLParam(r, "id"))
+		id := chi.URLParam(r, "id")
+
+		if id == "" {
+			http.Error(w, s.Config.RequestNotAllowedError, http.StatusBadRequest)
+			return
+		}
+
+		storageURL, err := s.Storage.Get(id)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		http.Redirect(w, r, uri, http.StatusTemporaryRedirect)
+
+		validURL, err := url.ParseRequestURI(storageURL)
+		if err != nil {
+			http.Error(w, s.Config.RequestNotAllowedError, http.StatusBadRequest)
+			return
+		}
+
+		http.Redirect(w, r, validURL.String(), http.StatusTemporaryRedirect)
 	}
 }
